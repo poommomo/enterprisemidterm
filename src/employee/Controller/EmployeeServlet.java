b925376b5557657cf5169c6ceb5621638db5d6ca
poommomo <perlmomo@gmail.com>
2018-10-04 12:34:48 +0700

    add database connection

---
+++ src/employee/Controller/EmployeeServlet.java
@@ -0,0 +1,28 @@
+package employee.Controller;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EmployeeServlet extends HttpServlet {
+
+    public void init() throws ServletException {
+
+    }
+
+
+
+
+
+}
---
+++ src/employee/Employee.java
@@ -0,0 +1,42 @@
+package employee;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String hireDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+}
